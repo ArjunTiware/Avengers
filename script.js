92d164b93ae20a81f738f764f991cefccf03f4fa
Arjun Tiwari <126322848+ArjunTiware@users.noreply.github.com>
2024-09-23 09:27:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+$(".custom-carousel").owlCarousel({
+    autoWidth: true,
+    loop: true,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true
+  });
+  $(document).ready(function () {
+    $(".custom-carousel .item").click(function () {
+      $(".custom-carousel .item").not($(this)).removeClass("active");
+      $(this).toggleClass("active");
+    });
+  });
+  
