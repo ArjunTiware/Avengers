a769083b83c6c78c2b2afccd55e7318a4580fca6
ArjunTiware <at8949681@gmail.com>
2024-09-23 10:33:48 +0530

    added new index.html file

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 $(".custom-carousel").owlCarousel({
-    autoWidth: true,
-    loop: true,
-    autoplay:true,
-    autoplayTimeout:3000,
-    autoplayHoverPause:true
+  autoWidth: true,
+  loop: true,
+  autoplay: true,
+  autoplayTimeout: 3000,
+  autoplayHoverPause: true
+});
+$(document).ready(function () {
+  $(".custom-carousel .item").click(function () {
+    $(".custom-carousel .item").not($(this)).removeClass("active");
+    $(this).toggleClass("active");
   });
-  $(document).ready(function () {
-    $(".custom-carousel .item").click(function () {
-      $(".custom-carousel .item").not($(this)).removeClass("active");
-      $(this).toggleClass("active");
-    });
-  });
-  
+});
